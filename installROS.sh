99dfdde830d47f7f7830eaabffea42317891d5df
dcofer <dcofer@NeuroRoboticTech.com>
2017-04-04 05:44:37 -0500

    Modified install ros to switch from indigo to kinetic

--- installROS.sh
+++ installROS.sh
@@ -13,15 +13,22 @@ sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB0
 # Installation
 sudo apt-get update
 sudo apt-get install ros-kinetic-ros-base -y
+
 # Add Individual Packages here
-# You can install a specific ROS package (replace underscores with dashes of the package name):
-# sudo apt-get install ros-kinetic-PACKAGE
-# e.g.
-# sudo apt-get install ros-kinetic-navigation
-#
-# To find available packages:
-# apt-cache search ros-kinetic
-# 
+sudo apt-get install ros-kinetic-desktop-full -y
+
+sudo apt-get install ros-kinetic-ar-track-alvar-msgs \
+  ros-kinetic-collada-urdf ros-kinetic-perception \
+  ros-kinetic-mrpt-navigation ros-kinetic-gmapping \
+  ros-kinetic-xacro -y
+
+sudo apt-get install ros-kinetic-ros-control \
+  ros-kinetic-gazebo-ros-control ros-kinetic-joint-state-controller \
+  ros-kinetic-effort-controllers ros-kinetic-joint-trajectory-controller \
+  ros-kinetic-moveit* ros-kinetic-octomap* ros-kinetic-object-recognition-* \
+  ros-kinetic-ros-tutorials
+
+
 # Initialize rosdep
 sudo apt-get install python-rosdep -y
 # ssl certificates can get messed up on TX1 for some reason
